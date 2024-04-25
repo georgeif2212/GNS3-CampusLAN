0d505580efadb3a0f1e5f694a9f3d7207220bba7
georgeif2212 <jinfante2212@gmail.com>
2024-04-25 17:40:59 -0600

    feat: server js file to load the server

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import http from "http";
+import app from "./app.js";
+import config from "./config/config.js";
+
+const server = http.createServer(app);
+
+const PORT = config.port;
+
+server.listen(PORT, () => {
+  console.log(`Server running in http://localhost:${PORT} 🤩`);
+});
