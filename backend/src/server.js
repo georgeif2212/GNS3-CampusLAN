953eaf1749fd47755e79dfbf285e99df382cc084
georgeif2212 <jinfante2212@gmail.com>
2024-04-27 13:37:22 -0600

    style: comment added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 import http from "http";
-import app from "./app.js";
-import config from "./config/config.js";
+import app from "./app.js"; 
+import config from "./config/config.js"; 
 
-const server = http.createServer(app);
+const server = http.createServer(app); // * Crea el servidor HTTP con tu app
 
 const PORT = config.port;
 
 server.listen(PORT, () => {
-  console.log(`Server running in http://localhost:${PORT} 🤩`);
+  console.log(`Server running at http://localhost:${PORT} 🤩`);
 });
