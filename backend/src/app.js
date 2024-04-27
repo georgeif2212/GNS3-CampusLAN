d05a70aa2d4a6f91998d2ff979db56025229ac70
georgeif2212 <jinfante2212@gmail.com>
2024-04-27 13:41:03 -0600

    feat: next and express configuration implemented

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,34 @@
 import express from "express";
+import next from "next";
+import config from "./config/config.js";
 
+import apiRouter from "./routers/api/api.router.js";
+
+const dev = config.env;
 const app = express();
+const nextApp = next({ dev }); // Crea la aplicación Next.js
+const handle = nextApp.getRequestHandler(); // Obtiene el manejador de Next.js
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello fucking world, this gonna be the best project ever")
+// Prepara Next.js y luego inicializa tus rutas y middleware personalizados
+nextApp.prepare().then(() => {
+
+  app.get("/home", (req, res) => {
+    return nextApp.render(req, res, "/home"); 
+  });
+
+  app.get("/products", (req, res) => {
+    return nextApp.render(req, res, "/products"); 
+  });
+
+  app.use("/api", apiRouter);
+
+  // * Maneja las demás rutas con Next
+  app.all("*", (req, res) => {
+    return handle(req, res);
+  });
 });
 
 export default app;
