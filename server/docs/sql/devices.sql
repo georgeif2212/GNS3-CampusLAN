45f00cd83159c12c8593f34bc06168b24f679d69
John Doe <john.doe@example.com>
2024-05-14 21:24:54 +0000

    feat: sql script to create devices table

---
+++ server/docs/sql/devices.sql
@@ -0,0 +1,7 @@
+CREATE TABLE devices (
+    DispositivoID INT PRIMARY KEY IDENTITY,
+    NombreHost VARCHAR(50),
+    VersionSoftware VARCHAR(50),
+    Modelo VARCHAR(50),
+    NumeroSerie VARCHAR(50)
+);
