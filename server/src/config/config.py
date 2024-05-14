1323d121b86e9a042cfc53c46436c47444273425
John Doe <john.doe@example.com>
2024-05-14 21:44:57 +0000

    feat: python script with function to load .env variables

--- server/src/config/config.py
+++ server/src/config/config.py
@@ -1,4 +1,15 @@
 from dotenv import load_dotenv
+import os
+
 
 def dotenv():
     load_dotenv()
+
+
+configVariables = {
+    "SERVER": os.getenv("SERVER"),
+    "DATABASE": os.getenv("DATABASE"),
+    "USERNAME": os.getenv("USERNAME"),
+    "PASSWORD": os.getenv("PASSWORD"),
+    "DRIVER": os.getenv("DRIVER"),
+}
