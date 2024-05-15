4aa93321b3f0de2a0bff60f66599241b9014f5be
John Doe <john.doe@example.com>
2024-05-15 00:31:46 +0000

    feat: new function to make request with reequests pytn module

--- server/src/utils/utils.py
+++ server/src/utils/utils.py
@@ -1,14 +1,40 @@
-default_query_ip="192.168.122.202"
-query_base="/restconf/data/Cisco-IOS-XE"
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+default_query_ip = "192.168.122.202"
+query_base = "/restconf/data/Cisco-IOS-XE"
+
+# Datos de autenticación
+username = os.getenv("USERNAME-GNS3-DEVICES")
+password = os.getenv("PWD-GNS3-DEVICES")
+
+# Encabezados de la solicitud
+headers = {"Accept": "application/yang-data+json"}
+
 
 def build_curl_command(ip_address, endpoint):
     command = [
         "sudo",
         "curl",
-        "--interface", "virbr0",
+        "--interface",
+        "virbr0",
         f"https://{ip_address}{query_base}{endpoint}",
         "-k",  # Ignorar verificación de certificados SSL
-        "-u", "admin:admin",  # Usuario y contraseña
-        "-H", "Accept: application/yang-data+json"
+        "-u",
+        "admin:admin",  # Usuario y contraseña
+        "-H",
+        "Accept: application/yang-data+json",
     ]
     return command
+
+
+def build_request_command(ip_address, endpoint):
+    return requests.get(
+        f"https://{ip_address}{query_base}{endpoint}",
+        headers=headers,
+        auth=(username, password),
+        verify=False,
+    )
