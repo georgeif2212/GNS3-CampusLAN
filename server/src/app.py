2dab24262c381e5b217c1e4c0852b6e6f9d266e1
georgeif2212 <jinfante2212@gmail.com>
2024-06-08 22:34:02 +0000

    feat: blueprint arp router added to endpoints

--- server/src/app.py
+++ server/src/app.py
@@ -6,6 +6,7 @@ from routers.api.devices_router import devices_router
 from routers.api.interfaces_router import interfaces_router
 from routers.api.queries_router import queries_router
 from routers.reports.reports_router import reports_router
+from routers.api.arp_router import arp_router
 
 from bfs import bfs_algorithm
 from werkzeug.exceptions import HTTPException
@@ -16,6 +17,7 @@ app = Flask(__name__)
 app.register_blueprint(reports_router, url_prefix="/reports")
 app.register_blueprint(devices_router, url_prefix="/api/devices")
 app.register_blueprint(interfaces_router, url_prefix="/api/interfaces")
+app.register_blueprint(arp_router, url_prefix="/api/arp")
 app.register_blueprint(queries_router, url_prefix="/api")
 
 
