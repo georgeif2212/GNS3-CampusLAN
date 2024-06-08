61136b0f08da90f967c9b975b27813085bd9851b
georgeif2212 <jinfante2212@gmail.com>
2024-06-08 22:35:14 +0000

    feat: new arp model

---
+++ server/src/model/arp_model.py
@@ -0,0 +1,64 @@
+from db.sql_server import cursor as db_cursor
+from utils.utils import build_fail_response_create, build_success_response_create
+
+
+class ArpModelSQL:
+    @staticmethod
+    def get(criteria=None):
+        query = "SELECT * FROM arp"
+        if criteria:
+            query += f" WHERE {criteria}"
+        db_cursor.execute(query)
+
+        result = db_cursor.fetchall()
+        column_description = db_cursor.description
+
+        return result, column_description
+
+    @staticmethod
+    def get_by_hostname(hostname):
+        query = "SELECT id_device FROM devices WHERE hostname = ?"
+        db_cursor.execute(query, (hostname))
+        result = db_cursor.fetchone()
+        if result:
+            return {"id_device": result[0]}
+        return None
+
+    @staticmethod
+    def create(data):
+        print(f"data: {data}")
+        query = "INSERT INTO devices (hostname, software_version, model, serial_number) VALUES (?, ?, ?, ?)"
+        db_cursor.execute(
+            query,
+            (
+                data["nombre_host"],
+                data["version_software"],
+                data["modelo"],
+                data["numero_serie"],
+            ),
+        )
+        db_cursor.commit()
+
+        # Obtain the number of affected rows
+        rows_affected = db_cursor.rowcount
+
+        if rows_affected > 0:
+            print("Successful insertion")
+            return build_success_response_create
+        else:
+            print("Failed insertion")
+            return build_fail_response_create
+
+    @staticmethod
+    def update_by_id(did, data):
+        query = "UPDATE devices SET name = ?, price = ? WHERE id = ?"
+        db_cursor.execute(query, (data["name"], data["price"], did))
+        db_cursor.commit()
+        return True  # Assuming successful update
+
+    @staticmethod
+    def delete_by_id(did):
+        query = "DELETE FROM devices WHERE id = ?"
+        db_cursor.execute(query, (did,))
+        db_cursor.commit()
+        return True  # Assuming successful deletion
