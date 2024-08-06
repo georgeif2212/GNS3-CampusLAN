51d24f76b48543b8664de958d0938c5e71a88d5c
georgeif2212 <jinfante2212@gmail.com>
2024-08-06 00:11:28 +0000

    fix: the software_version field should have been TEXT instead VARCHAR

--- server/docs/sql/software.sql
+++ server/docs/sql/software.sql
@@ -3,7 +3,7 @@ CREATE TABLE software (
     id_device UNIQUEIDENTIFIER,
     current_time_device VARCHAR(30),
     boot_time VARCHAR(30),
-    software_version VARCHAR(50),
+    software_version TEXT,
     rommon_version VARCHAR(20),
     last_reboot_reason VARCHAR(20),
     FOREIGN KEY (id_device) REFERENCES devices(id_device)
