970066b2a21719fa5b128b3929aa69308f239ba5
georgeif2212 <jinfante2212@gmail.com>
2024-08-06 00:10:41 +0000

    feat: SQL Script to create software table

---
+++ server/docs/sql/software.sql
@@ -0,0 +1,10 @@
+CREATE TABLE software (
+    id_software UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+    id_device UNIQUEIDENTIFIER,
+    current_time_device VARCHAR(30),
+    boot_time VARCHAR(30),
+    software_version VARCHAR(50),
+    rommon_version VARCHAR(20),
+    last_reboot_reason VARCHAR(20),
+    FOREIGN KEY (id_device) REFERENCES devices(id_device)
+);
